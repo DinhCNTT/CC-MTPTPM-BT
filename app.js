ff527896d3b6f542e19d9fca33e70bce364f634f
DinhCNTT <dinhcm123321@gmail.com>
2026-03-17 14:46:06 +0700

    Fix syntax error in app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use((req, res, next) => {
-    console.log(${req.method} );
-    next();
+  console.log(`${req.method} ${req.url}`);
+  next();
 });
 // Serve static files from public folder
 app.use(express.static(path.join(__dirname, 'public')));
